9d4b476325ae8a5e89fa450c7021f7de54c1b9ed
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-07-18 19:35:00 -0300

    Testando o componente About.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,28 @@
-test('iniciando o projeto', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import About from '../pages/About';
+
+describe('Router', () => {
+  test('Test if there is h2', () => {
+    render(<About />);
+
+    const aboutPokedex = screen.getByRole('heading',
+      { name: /About Pokédex/i, level: 2 });
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+
+  test('Test if there are two same Pokeédex', () => {
+    render(<About />);
+
+    const pokedex = screen.getAllByText(/Pokédex/i);
+    expect(pokedex).toBeDefined();
+  });
+
+  test('Test if there is a picture0', () => {
+    render(<About />);
+
+    const altImg = screen.getByAltText('Pokédex');
+    expect(altImg).toHaveAttribute('src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,19 +4,29 @@ import React from 'react';
 import { MemoryRouter, Router } from 'react-router-dom';
 import App from '../App';
 
-describe('Rotas', () => {
-  test('testando o conjunto de links de navegação', () => {
+describe('Router', () => {
+  test('Test render home', () => {
     render(<MemoryRouter><App /></MemoryRouter>);
 
     const homeLink = screen.getByRole('link', { name: /home/i });
     expect(homeLink).toBeInTheDocument();
+  });
+
+  test('Test render about', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+
     const aboutLink = screen.getByRole('link', { name: /about/i });
     expect(aboutLink).toBeInTheDocument();
+  });
+
+  test('Test render favorites', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+
     const favoriteLink = screen.getByRole('link', { name: /favorite/i });
     expect(favoriteLink).toBeInTheDocument();
   });
 
-  test('testando renderização de página não encontrada', () => {
+  test('Test render not found', () => {
     const history = createMemoryHistory();
     render(
       <Router history={ history }>
