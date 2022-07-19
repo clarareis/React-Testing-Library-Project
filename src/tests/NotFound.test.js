492e4cfbb89eb1fe0c02a295d83d8cdf3c6ce904
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-07-19 09:34:36 -0300

    Testando o componente NotFound.

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import NotFound from '../pages/NotFound';
+
+describe('Testing component NotFound', () => {
+  test('Test if there is h2', () => {
+    render(<NotFound />);
+
+    const aboutPokedex = screen.getByRole('heading',
+      { name: /Page requested not found/i, level: 2 });
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+
+  test('Test if there is picture', () => {
+    render(<NotFound />);
+    const altImg = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(altImg).toHaveAttribute('src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
