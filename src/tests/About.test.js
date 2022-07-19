824d5bfd066d648bd231a84939c6e9912a243eb7
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-07-18 21:04:18 -0300

    Vertificando o componente FavoritePokemons.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import About from '../pages/About';
 
-describe('Router', () => {
+describe('Testing component About', () => {
   test('Test if there is h2', () => {
     render(<About />);
 
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Testing component FavoritePokemons', () => {
+  test('Test if there are not favorites pokemons ', () => {
+    render(<FavoritePokemons />);
+
+    const notFavoritepokemon = screen.getByText(/No favorite pokemon found/i);
+    expect(notFavoritepokemon).toBeInTheDocument();
+  });
+
+  test('Test if show all favorites pokemons ', () => {
+    const { container } = render(<FavoritePokemons />);
+
+    const favoritepokemon = container.getElementsByClassName('favorite-pokemon');
+    expect(favoritepokemon.legth).toBeFalsy();
+  });
+});
