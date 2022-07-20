55f11212a553634b381b2661631cdceea13c4458
Maria Clara Reis <maria_clara_reis@hotmail.com>
2022-07-20 09:47:32 -0300

    Conseguindo resolver o 5 requisito - VQV - O futuro é ser QA -:party:

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import { fireEvent, render, screen } from '@testing-library/react';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+
+describe('Testing component Pokedex', () => {
+  test('Test if there is h2', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+
+    const pokedexText = screen.getByRole('heading',
+      { name: /Encountered pokémons/i, level: 2 });
+    expect(pokedexText).toBeInTheDocument();
+  });
+
+  test('Test if there is a button for reset', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+
+    const pokedexFilter = screen.getByRole('button', { name: 'All' });
+    fireEvent.click(pokedexFilter);
+    const reset = screen.getAllByTestId('pokemon-type-button');
+    expect(reset).toBeDefined();
+  });
+
+  test('Test if there a pokemon', () => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+
+    const pokemonCard = screen.getByRole('button', { name: /fire/i });
+    fireEvent.click(pokemonCard);
+    const showPokemon = screen.getAllByTestId('pokemon-type-button');
+    expect(showPokemon).toBeDefined();
+  });
+});
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+// import App from '../App';
+import Pokemon from '../components/Pokemon';
+
+describe('Testing component Pokemon', () => {
+  test('Test link navegation on click ', () => {
+    render(<Pokemon />);
+
+    const pokemonLink = screen.getByRole('link', { name: ' /pokemons/25' });
+    expect(pokemonLink).toBeInTheDocument();
+  });
+});
